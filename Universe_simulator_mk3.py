8e4424aebdaa817ea103f917dfe7071c6eeab301
TheJDevK <jayadev.kamath@gmail.com>
2026-06-12 15:50:15 +0530

    Phase 3A: New universe architecture

--- Universe_simulator_mk3.py
+++ Universe_simulator_mk3.py
@@ -1,26 +1,136 @@
 #information
-#Phase 2A:Orbital mechanics: successful. Newtonian mechanics for two bodies in space worked.
-#Phase 2B:N-body simulation: Successful.
-#Phase 2C:Front-end development: Successful, stable version released.
+#Phase 2A:Orbital mechanics: successful. Newtonian mechanics for two bodies in space worked. Verlet integration works properly.
+#Phase 2B:N-body simulation: Successful. Now can add multiple bodies to the same universe
+#Phase 2C:Front-end development: Successful. Simulation window made better with subtle changes.
+#Phase 3A:improving architecture and universe class. New JKM architecture has made code more organised and reduced number of lines by nearly 15% while doing the same thing.: stable version pending.
 
 
 import numpy as np
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
-rows = []
 
-#essential constants
-DT = 0.01
-MAX_TIME = 16.0
-current_time = 0.0
-e = 1.0 #Elasticity
-collision = 0
-G = 1  #currently experimental, so G taken as 1. #unibersal gravitational constant = 6.6743e-11
-objects = []
+#universe class
+class Universe:
+    def __init__(self, dt, max_time, G, verbose):
+
+        self.dt = dt
+        self.max_time = max_time
+        self.G = G
+        self.verbose = verbose
+        self.time = 0.0
+        self.objects = []
+        self.rows = []
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def total_energy(self):
+        PE = 0
+
+        for a in range(len(self.objects)):
+            for b in range(a + 1, len(self.objects)):
+                r_vec = self.objects[b].pos - self.objects[a].pos
+                r = np.linalg.norm(r_vec)
+                PE += -(self.G * self.objects[a].mass * self.objects[b].mass) / r
+
+        KE = sum(o.K_energy for o in self.objects)
+        E = KE + PE
+        print("Total Energy = ", E)
+
+    def total_momentum(self):
+        p = np.array([0.0, 0.0])
+        for o in self.objects:
+            p += o.momentum
+        print("Momentum:", p)
+
+    def apply_gravity(self):
+
+        for i in self.objects:
+            i.acc[:] = 0
+
+        for i in self.objects:
+            for j in self.objects:
+                if i == j:
+                    continue
+                r_vec = j.pos - i.pos
+                r = np.linalg.norm(r_vec)
+                if r == 0:   #divison by 0 prevented
+                    continue
+                i.acc += (
+                        self.G
+                        * j.mass
+                        * r_vec
+                        / r ** 3
+                )
+
+    def integrate(self):
+
+        old_acc = [o.acc.copy() for o in self.objects]
+
+        for o, a in zip(self.objects, old_acc):
+            o.pos += (o.vel * self.dt + 0.5 * a * self.dt ** 2)
+
+        self.apply_gravity()
+
+        for o, a in zip(self.objects, old_acc):
+            o.vel += ( 0.5 * (a + o.acc) * self.dt)
+
+    def logger(self):
+
+        for o in self.objects:
+            if self.verbose:
+                print(
+                    f" {o.name} t={self.time:.2f}s  "
+                    f"pos = ({o.pos[0]:.2f}, {o.pos[1]:.2f})  "
+                    f"vel = ({o.vel[0]:.2f}, {o.vel[1]:.2f})  "
+                    f"p = {o.momentum}  "
+                    f"KE = {o.K_energy:.2f}"
+                    f"Acc = {o.acc}"
+                )
+
+        snapshot = {"t": self.time}
+
+        for body in self.objects:
+            snapshot[f"{body.name}_x"] = body.pos[0]
+            snapshot[f"{body.name}_y"] = body.pos[1]
+        self.rows.append(snapshot)
+
+        # -----Old dataframe design. Use later-----
+        # rows.append({
+        #    "t": current_time,
+        #    "x1": ball1.pos[0],
+        #    "y1": ball1.pos[1],
+        #    "v1": ball1.vel[0],
+        #    "x2": ball2.pos[0],
+        #    "y2": ball2.pos[1],
+        #    "v2": ball2.vel[0],
+        #    "p_total": (ball1.momentum[0] + ball2.momentum[0]),
+        #    "KE_total": ball1.K_energy + ball2.K_energy,
+        #    "collided": collision,  # 0 or 1
+        # })
+        # -----------------------------------------
+
+    def step(self):
+
+        self.integrate()
+        self.logger()
+        self.time += self.dt
+
+    def dataframe(self):
+
+        return pd.DataFrame(self.rows)
+
+    def run(self):
+
+        self.apply_gravity()
+        while self.time < self.max_time:
+            self.step()
+
+
 
 #class to create objects in this universe
-class Uobjects:
+class Body:
     def __init__(self, name, mass, radius, x, y, vx, vy, ax, ay):
         self.name = name
         self.mass = mass
@@ -50,154 +160,9 @@ class Uobjects:
     def K_energy(self):
         return 0.5 * self.mass * np.sum(self.vel**2)
 
-#methods for objects
-
-def total_energy(obj):
-    PE = 0
-
-    for a in range(len(obj)):
-        for b in range(a + 1, len(obj)):
-            r_vec = obj[b].pos - obj[a].pos
-            r = np.linalg.norm(r_vec)
-
-            PE += -(G * obj[a].mass * obj[b].mass) / r
-
-    KE = sum(o.K_energy for o in obj)
-
-    E = KE + PE
-    print("Total Energy = ", E)
-
-def total_momentum(obj):
-    p = np.array([0.0, 0.0])
-    for o in obj:
-        p += o.momentum
-    print("Momentum:", p)
-
-
-#1) New updated N-body gravity engine. No credits to Pollov
-def apply_gravity(obj):
-
-    for i in obj:
-        i.acc[:] = 0
-
-    for i in obj:
-        for j in obj:
-            if i == j:
-               continue
-
-            r_vec = j.pos - i.pos
-            r = np.linalg.norm(r_vec)
-            if r == 0:
-                continue
-
-            i.acc += (G * j.mass * r_vec / r ** 3)
-
-    #-----Old 2-body simulation code.Saved for reference-----
-    #direction = r_vec / r
-    #G_force = (G * obj[0].mass * obj[1].mass) / r ** 2
-    #a1 = G_force / obj[0].mass
-    #a2 = G_force / obj[1].mass
-    #obj[0].acc = a1 * direction
-    #obj[1].acc = -a2 * direction
-    #--------------------------------------------------------
-
-#2)Velocity and position handler. Currently, handles only two objects.
-def integrate(obj, dt):
-    #Euler integration
-    # 1. update velocities (if any accel)
-    #obj[0].vel += obj[0].acc * dt
-    #obj[1].vel += obj[1].acc * dt
-    # problematic code here
-    # 2. update positions
-    #obj[0].pos += obj[0].vel * dt
-    #obj[1].pos += obj[1].vel * dt
-
-    #NEW UPDATE VERLET INTEGRATION
-    #Creates a list of old accelerations of all objects present
-    old_acc = [o.acc.copy() for o in obj]
-
-    for o, a in zip(obj, old_acc):
-        o.pos += o.vel * dt + 0.5 * a * dt * dt
-
-    apply_gravity(obj)
-
-    for o, a in zip(obj, old_acc):
-        o.vel += 0.5 * (a + o.acc) * dt
-
-
-#3)Collision handler. UNDER DEVELOPMENT.
-def resolve_collision(obj):
-    b1 = obj[0]
-    b2 = obj[1]
-    relative_pos = b1.pos - b2.pos
-    distance = np.linalg.norm(relative_pos)
-    if distance <= (b1.radius + b2.radius):
-        normal = relative_pos/distance
-        relative_vel = b1.vel - b2.vel
-        #Velocity Along Normal
-        van = np.dot(relative_vel, normal)
-        if van > 0:
-            return False
-
-    if abs(b1.pos[0] - b2.pos[0]) <= (b1.radius + b2.radius):
-        collision = 1
-        print("Collision!")
-        # (a) snap them so they are just touching, centered around midpoint
-        mid = 0.5 * (b1.pos[0] + b2.pos[0])
-        b1.pos[0] = mid - b1.radius
-        b2.pos[0] = mid + b2.radius
-        # (b) swap their x-velocities (equal masses, 1D, elastic)
-        u1 = b1.vel[0]
-        u2 = b2.vel[0]
-        m1 = b1.mass
-        m2 = b2.mass
-        den = m1 + m2
-        v1 = ((m1 - m2) / den) * u1 + ((2 * m2) / den) * u2
-        v2 = ((2 * m1) / den) * u1 + ((m2 - m1) / den) * u2
-        b1.vel[0] = v1
-        b2.vel[0] = v2
-
-        # handle collision here: snap positions, swap velocities, etc.
-
-#4)Data logger
-def logger(obj):
-
-    for o in obj:
-        print(
-            f" {o.name} t={current_time:.2f}s  "
-            f"pos = ({o.pos[0]:.2f}, {o.pos[1]:.2f})  "
-            f"vel = ({o.vel[0]:.2f}, {o.vel[1]:.2f})  "
-            f"p = {o.momentum}  "
-            f"KE = {o.K_energy:.2f}"
-            f"Acc = {o.acc}"
-        )
-
-    snapshot = {"t": current_time}
-
-    for body in obj:
-       snapshot[f"{body.name}_x"] = body.pos[0]
-       snapshot[f"{body.name}_y"] = body.pos[1]
-
-    rows.append(snapshot)
-
-    #-----Old dataframe design. Use later-----
-    #rows.append({
-    #    "t": current_time,
-    #    "x1": ball1.pos[0],
-    #    "y1": ball1.pos[1],
-    #    "v1": ball1.vel[0],
-    #    "x2": ball2.pos[0],
-    #    "y2": ball2.pos[1],
-    #    "v2": ball2.vel[0],
-    #    "p_total": (ball1.momentum[0] + ball2.momentum[0]),
-    #    "KE_total": ball1.K_energy + ball2.K_energy,
-    #    "collided": collision,  # 0 or 1
-    #})
-    #-----------------------------------------
-
 
 #5)Visual engine
-def simulator_visual(obj, df):
+def simulator_visual(universe, df):
     # plotter engine
     #Giving colours and rendering sizes of objects. Not real radius - only rendered.
     #Rendering is for temporary visual testing.
@@ -208,7 +173,7 @@ def simulator_visual(obj, df):
     plt.style.use("dark_background")
     #size of canvas
     fig, ax = plt.subplots(figsize=(10, 10))
-    for body in obj:
+    for body in universe.objects:
 
         #Centre the star. change this to see how planet and moon's gravity affects the star.
         star_x = df["Star1_x"]
@@ -241,13 +206,13 @@ def simulator_visual(obj, df):
         )
     #title
     ax.set_title(
-        f"N-Body Simulation\nBodies: {len(obj)}",
+        f"N-Body Simulation\nBodies: {len(universe.objects)}",
         fontsize=14
     )
 
     stats = (
-        f"dt = {DT}\n"
-        f"Time = {MAX_TIME}\n"
+        f"dt = {universe.dt}\n"
+        f"Time = {universe.max_time}\n"
         f"Steps = {len(df)}"
     )
     #adding text to the canvas
@@ -264,12 +229,12 @@ def simulator_visual(obj, df):
     ax.set_xticks([])
     ax.set_yticks([])
 
-    #this part removes the boders
+    #this part removes the borders
     ax.spines['top'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    #set_aspect makes units equal. Scaling is done properly. Otherwise orbit would be circle and not ellpise
+    #set_aspect makes units equal. Scaling is done properly. Otherwise orbit would be circular and not elliptical.
     ax.set_aspect("equal")
     #Legend builds the box with star1, planet1 and moon1
     ax.legend()
@@ -278,32 +243,20 @@ def simulator_visual(obj, df):
 
 
 #Universe creator
-Star =   Uobjects("Star1", 10000.0, 2.0, 40.0, 40.0, 0, 0, 0, 0)
-Planet = Uobjects("Planet1", 100.0,0.5 , 20.0, 10.0, 17.6, 0, 0, 0)
-Moon =   Uobjects("Moon1", 1.0, 0.1, 21.0, 10.0, 17.6, 10, 0, 0)
+universe = Universe(dt = 0.01, max_time = 16.0, G = 1.0, verbose = False)
 
-objects.append(Star)
-objects.append(Planet)
-objects.append(Moon)
+#Object creator
+#Introducing the objects to reality in the universe
+universe.add_object(Body("Star1", 10000.0, 2.0, 40.0, 40.0, 0, 0, 0, 0))
+universe.add_object(Body("Planet1", 100.0,0.5 , 20.0, 10.0, 17.6, 0, 0, 0))
+universe.add_object(Body("Moon1", 1.0, 0.1, 21.0, 10.0, 17.6, 10, 0, 0))
 #x_wall = 2.0 #simple wall
 
-apply_gravity(objects)
-for o in objects:
-    print(o.name, o.acc)
-print("Acceleration magnitude:", np.linalg.norm(o.acc))
-while current_time < MAX_TIME:
-    integrate(objects, DT)
-    #   resolve_collision(objects)
-    logger(objects)
-    total_energy(objects)
-    total_momentum(objects)
-    current_time = current_time + DT
+universe.run()
 
-
-df = pd.DataFrame(rows)
+df = universe.dataframe()
 print(df.head())
 
-simulator_visual(objects, df)
+simulator_visual(universe, df)
 
 #print(df[df["collided"] == 1])
-
